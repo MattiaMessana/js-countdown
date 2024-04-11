1529e38409364c9fe7f639ddebcaa29b2abab882
MattiaMessana <mattia.messana@icloud.com>
2024-04-11 16:26:23 +0200

    add: aggiunto html ed eventListener sul bottone per far partire il countdown

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Simulare un countdown di 10 secondi che alla fine dice "Buon anno!"
 Iniziate stampando il countdown nella console e dopo stampatelo in pagina.
 
-creiamo un timer di 10 secondi 
+creiamo un countDown di 10 secondi 
     faccimo partire il timer da 10  e lo facciamo arrivare a 0 
-        appena arriva a zero 
+        se arriva a zero 
            stampo la scritta "buon anno"
 
 
--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-//creo un timer di 10 secondi 
-const countDown = setTimeout(function () {
-    
+//variabili 
+let seconds = 11;
+const timeDisplay = document.getElementById("countdown");
+
+// individuo il bottone in pagina 
+const startBtn = document.getElementById("start");
+
+//eseguiamo la funziona al click sul bottone 
+startBtn.addEventListener("click" , function() {
+    //creo un countDown di 10 secondi 
+const countDown = setInterval(function(){
+    seconds--;
+    if (seconds === 0) {
+        console.warn('Buon Anno');
+        clearInterval(countDown);
+    }
+    timeDisplay.innerHTML += ` 
+    <div> ${seconds} </div>`
 }, 1000);
+})
